e7b84505a08a3b7fabb0d93c908732dde7804bac
RyanChew1 <113740030+RyanChew1@users.noreply.github.com>
2023-02-12 01:09:32 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+from flask_socketio import SocketIO, join_room, leave_room, emit
+from flask_session import Session
 import mysql.connector
+import random
+import string
 
 app = Flask(__name__, template_folder='templates')
+app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(app)
+
+socketio = SocketIO(app, manage_session=False)
 
 mydb = mysql.connector.connect(
   host="127.0.0.1",
@@ -17,7 +26,7 @@ app.secret_key = 'secret_key'
 def index():
     print(session)
     if 'username' in session:
-        return render_template('logged_in.html')
+        return render_template('logged_in.html',visibility = 'none')
     else:
         return render_template('index.html')
 
@@ -78,6 +87,7 @@ def logout():
     session.clear()
     username = None
     return redirect(url_for('index'))
+
 # game modes
 @app.route('/standard.html', methods=['GET', 'POST'])
 def standard():
@@ -99,7 +109,107 @@ def sailing():
 def advanced():
     return render_template("advanced.html")
 
+
+
+
+# # START
+def dropEmpty():
+    cursor = mydb.cursor()
+    query = "DELETE FROM rooms WHERE numUsers = 0"
+    cursor.execute(query)
+    mydb.commit()
+
+def createNew(room):
+    cursor = mydb.cursor()
+    query = "INSERT INTO rooms (roomcode,numUsers) VALUE (%s, 1)"
+    cursor.execute(query,(room,))
+    mydb.commit()
+
+def adduser(room):
+    cursor = mydb.cursor()
+
+    queryNum = "SELECT numUsers FROM rooms WHERE roomcode = %s"
+    cursor.execute(queryNum, (room,))
+    num = cursor.fetchone()[0]
+    print(f"num: {num}")
+    query = "UPDATE rooms SET numUsers = %s WHERE roomcode=%s"
+    cursor.execute(query,(num+1,room))
+    mydb.commit()
+
+@app.route('/chat', methods=['GET', 'POST'])
+def chat():
+    if(request.method=='POST'):
+        cursor = mydb.cursor()
+        room = request.form['room']
+
+
+        query = "SELECT numUsers FROM rooms WHERE roomcode = %s"
+        cursor.execute(query, (room,))
+        result = cursor.fetchone()
+        if result:
+            session['room'] = room
+            adduser(room)
+            return render_template('logged_in.html', session = session,visibility = 'none')
+        else:
+            return render_template('logged_in.html', session = session,visibility = 'block')
+
+        
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if(request.method=='POST'):
+        cursor = mydb.cursor()
+        found = False
+        while not found:
+            room = ''
+            for i in range(8):
+                room+=random.choice(string.ascii_uppercase)
+            
+            query = "SELECT numUsers FROM rooms WHERE roomcode = %s"
+            cursor.execute(query, (room,))
+            result = cursor.fetchone()
+            if result:
+                pass
+            else:
+                found=True
+        dropEmpty()
+        session['room'] = room
+
+        createNew(room)
+
+        return render_template('logged_in.html', session = session,visibility = 'none')
+
+
+@socketio.on('join', namespace='/chat')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg': session.get('username') + ' : ' + message['msg']}, room=room)
+
+
+
+# SUBRTRACT USER
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = session.get('room')
+    username = session.get('username')
+    leave_room(room)
+    session['room'] = None
+    emit('status', {'msg': username + ' has left the room.'}, room=room)
+
+
+# # END
+
+
+
 if __name__ == '__main__':
-    app.run(host='10.0.0.31', port=5000, debug=True)
+    #app.run(host='10.0.0.31', port=5000, debug=True)
+    socketio.run(app=app,host='10.0.0.31', port=5000, debug=True)
 
 
