bd16a59ce70bbd7ef9779b1d79a5b3e519270bc5
RyanChew1 <113740030+RyanChew1@users.noreply.github.com>
2023-02-15 23:56:13 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_session import Session
 import mysql.connector
 import random
 import string
+import game
 
 app = Flask(__name__, template_folder='templates')
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -24,9 +25,11 @@ app.secret_key = 'secret_key'
 # redirect
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    session['room'] = session.get('room')
     print(session)
     if 'username' in session:
-        return render_template('logged_in.html',visibility = 'none')
+        session['room'] =None
+        return render_template('logged_in.html',visibility = 'none', session = session,vis2 = 'none')
     else:
         return render_template('index.html')
 
@@ -52,7 +55,7 @@ def signup():
             insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
             cursor.execute(insert_query, (username, password))
             mydb.commit()
-            return render_template("logged_in.html")
+            return render_template("logged_in.html", visibility='none')
     return render_template("signup.html", visibility='none')
 
 
@@ -72,7 +75,7 @@ def login():
         if result:
             # If the username and password are correct, redirect to the homepage
             session['username'] = username
-            return render_template("logged_in.html")
+            return render_template("logged_in.html", visibility='none',vis2 = 'none')
         elif password!='':
             # If the username and password are incorrect, show an error message
             session.clear()
@@ -88,10 +91,46 @@ def logout():
     username = None
     return redirect(url_for('index'))
 
+
+
+boardSize = {'a1V': 'none', 'b1V': 'none', 'c1V': 'none', 'd1V': 'none', 'e1V': 'none', 'f1V': 'none', 'g1V': 'none', 'h1V': 'none', 'i1V': 'none', 'j1V': 'none',
+            'a2V': 'none', 'b2V': 'none', 'c2V': 'none', 'd2V': 'none', 'e2V': 'none', 'f2V': 'none', 'g2V': 'none', 'h2V': 'none', 'i2V': 'none', 'j2V': 'none',
+            'a3V': 'none', 'b3V': 'none', 'c3V': 'none', 'd3V': 'none', 'e3V': 'none', 'f3V': 'none', 'g3V': 'none', 'h3V': 'none', 'i3V': 'none', 'j3V': 'none',
+            'a4V': 'none', 'b4V': 'none', 'c4V': 'none', 'd4V': 'none', 'e4V': 'none', 'f4V': 'none', 'g4V': 'none', 'h4V': 'none', 'i4V': 'none', 'j4V': 'none',
+            'a5V': 'none', 'b5V': 'none', 'c5V': 'none', 'd5V': 'none', 'e5V': 'none', 'f5V': 'none', 'g5V': 'none', 'h5V': 'none', 'i5V': 'none', 'j5V': 'none',
+            'a6V': 'none', 'b6V': 'none', 'c6V': 'none', 'd6V': 'none', 'e6V': 'none', 'f6V': 'none', 'g6V': 'none', 'h6V': 'none', 'i6V': 'none', 'j6V': 'none',
+            'a7V': 'none', 'b7V': 'none', 'c7V': 'none', 'd7V': 'none', 'e7V': 'none', 'f7V': 'none', 'g7V': 'none', 'h7V': 'none', 'i7V': 'none', 'j7V': 'none',
+            'a8V': 'none', 'b8V': 'none', 'c8V': 'none', 'd8V': 'none', 'e8V': 'none', 'f8V': 'none', 'g8V': 'none', 'h8V': 'none', 'i8V': 'none', 'j8V': 'none',
+            'a9V': 'none', 'b9V': 'none', 'c9V': 'none', 'd9V': 'none', 'e9V': 'none', 'f9V': 'none', 'g9V': 'none', 'h9V': 'none', 'i9V': 'none', 'j9V': 'none', 
+            'a10V': 'none', 'b10V': 'none', 'c10V': 'none', 'd10V': 'none', 'e10V': 'none', 'f10V': 'none', 'g10V': 'none', 'h10V': 'none', 'i10V': 'none', 'j10V': 'none',
+            'VA': 'none', 'VB': 'none', 'VC': 'none', 'VD': 'none', 'VE': 'none', 'VF': 'none', 'VG': 'none', 'VH': 'none', 'VI': 'none', 'VJ': 'none',
+            'V1': 'none', 'V2': 'none', 'V3': 'none', 'V4': 'none', 'V5': 'none', 'V6': 'none', 'V7': 'none', 'V8': 'none', 'V9': 'none', 'V10': 'none'}
+
+mapSize = {
+    1:'A',
+    2:'B',
+    3:'C',
+    4:'D',
+    5:'E',
+    6:'F',
+    7:'G',
+    8:'H',
+    9:'I',
+    10:'J'
+}
+
 # game modes
 @app.route('/standard.html', methods=['GET', 'POST'])
 def standard():
-    return render_template("standard.html",)
+    if request.method == 'POST':
+        rowSize = int(request.form['rowSize'])
+        colSize = int(request.form['colSize'])
+        #set display based on inputs
+        for i in boardSize:
+            pass
+        return render_template("standard.html",**boardSize)
+    else:
+        return render_template('standard.html',**boardSize)
 
 @app.route('/rapid.html', methods=['GET', 'POST'])
 def rapid():
@@ -136,22 +175,41 @@ def adduser(room):
     cursor.execute(query,(num+1,room))
     mydb.commit()
 
+def removeuser(room):
+    cursor = mydb.cursor()
+
+    queryNum = "SELECT numUsers FROM rooms WHERE roomcode = %s"
+    cursor.execute(queryNum, (room,))
+    print(f'ROOOM: {room}')
+    num = cursor.fetchone()[0]
+    print(f' NUM {num}')
+    query = "UPDATE rooms SET numUsers = %s WHERE roomcode=%s"
+    cursor.execute(query,(num-1,room))
+    mydb.commit()
+
+
+
+
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
     if(request.method=='POST'):
         cursor = mydb.cursor()
         room = request.form['room']
-
+        if session.get('room')==room:
+            return render_template('logged_in.html', session = session,visibility = 'none',vis2 = 'none')
 
         query = "SELECT numUsers FROM rooms WHERE roomcode = %s"
         cursor.execute(query, (room,))
         result = cursor.fetchone()
         if result:
-            session['room'] = room
-            adduser(room)
-            return render_template('logged_in.html', session = session,visibility = 'none')
+            if result[0]<2:
+                session['room'] = room
+                adduser(room)
+                return render_template('logged_in.html', session = session,visibility = 'none',vis2 = 'block')
+            else:
+                return render_template('logged_in.html', session = session, visibility='none',vis2 = 'block')
         else:
-            return render_template('logged_in.html', session = session,visibility = 'block')
+            return render_template('logged_in.html', session = session,visibility = 'block',vis2 = 'none')
 
         
 
@@ -160,6 +218,14 @@ def create():
     if(request.method=='POST'):
         cursor = mydb.cursor()
         found = False
+        print('#'*20)
+        print(session.get('room'))
+        if session.get('room') is not None:
+            removeuser(session.get('room'))
+            dropEmpty()
+            session['room'] = None
+
+
         while not found:
             room = ''
             for i in range(8):
@@ -177,14 +243,15 @@ def create():
 
         createNew(room)
 
-        return render_template('logged_in.html', session = session,visibility = 'none')
+        return render_template('logged_in.html', session = session,visibility = 'none',vis2 = 'none')
 
 
 @socketio.on('join', namespace='/chat')
 def join(message):
     room = session.get('room')
-    join_room(room)
-    emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
+    if session.get('username') is not None:
+        join_room(room)
+        emit('status', {'msg':  session.get('username') + ' has entered the room.'}, room=room)
 
 
 @socketio.on('text', namespace='/chat')
@@ -199,12 +266,21 @@ def text(message):
 def left(message):
     room = session.get('room')
     username = session.get('username')
-    leave_room(room)
+    if room:
+        removeuser(room)
+        leave_room(room)
+    
+
+    
+    dropEmpty()
     session['room'] = None
     emit('status', {'msg': username + ' has left the room.'}, room=room)
 
 
-# # END
+
+map_row = {
+    
+}
 
 
 
--- targetboard.py
+++ targetboard.py
@@ -12,39 +12,38 @@ from board import Board
 #    isHit(r, c) - is there a a "red peg" at (r, c)?
 #    isEmpty(r, c) - is there any peg at (r, c)?
 #=============================================================================
+
+
 class TargetBoard(Board):
 
     def __init__(self, rsize: int, csize: int):
         super().__init__(rsize, csize)
-        pass
 
     # place a "red peg" at (r, c)
     # "x" represents red peg
     def markHit(self, r: int, c: int) -> None:
         # put a red peg("x") at this location
         self.putPiece("x", r, c)
-        pass
 
     # place a "white peg" at (r, c)
     # "o" represents white peg
     def markMiss(self, r: int, c: int) -> None:
         # put a white peg("o") at this location
         self.putPiece("o", r , c)
-        pass
 
     # is there a "red peg" (a hit) at (r, c)?
     def isHit(self, r: int, c: int) -> bool:
         # if this location has a red peg("x") then return true
         if self.getPiece(r, c) == "x":
             return True
-        pass
+        return False
 
     # is there a any peg at (r, c)?
     def isEmpty(self, r: int, c: int) -> bool:
         # checks to see if there is a peg at this location
-        if self.getPiece(r, c) == "x" or self.getPiece(r, c) == "o":
+        if self.getPiece(r, c) is not None:
             return False
-        pass
+        return True
 
 
-    
+board = TargetBoard(10,10)
