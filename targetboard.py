24b8474c816397e0fdc40ec7d50fa89543a59177
RyanChew1 <113740030+RyanChew1@users.noreply.github.com>
2023-03-31 23:08:21 -0700

    Update targetboard.py

--- targetboard.py
+++ targetboard.py
@@ -1,17 +1,5 @@
 from board import Board
 
-#=============================================================================
-# Battleship Target Board - this is the vertical board in the Battleship
-# game that tracks hits and misses. In the game, when the player guesses and
-# hits, a red peg is placed at the coordinate. If it misses, a white peg
-# is placed at the coordinate.
-# 
-# Methods
-#    markHit(r, c) - place a "red peg" at (r, c)
-#    markMiss(r, c) - place a "white peg" at (r, c)
-#    isHit(r, c) - is there a a "red peg" at (r, c)?
-#    isEmpty(r, c) - is there any peg at (r, c)?
-#=============================================================================
 
 
 class TargetBoard(Board):
