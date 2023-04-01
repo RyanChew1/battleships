f57311e4509a32761f832159fdd2ae8b303fbb37
RyanChew1 <113740030+RyanChew1@users.noreply.github.com>
2023-03-31 23:06:40 -0700

    Update board.py

--- board.py
+++ board.py
@@ -1,14 +1,7 @@
-'''
-Board
 
-A generic 2D game board of a given dimension
-At each square in the game board, a piece can be placed onto the board
-The piece can be ANY type of item - strings, numbers, new GamePiece types...
-The board is initialized to None at the very beginning
-'''
 class Board:
 
-    ###### DO NOT EDIT ANYTHING IN THIS CLASS ######
+
     def __init__(self, rsize, csize):
         self.board = [ [ None for _ in range(csize) ] for _ in range(rsize) ]
 
@@ -28,8 +21,6 @@ class Board:
         for r in range(len(self.board)):
             for c in range(len(self.board[0])):
                 self.board[r][c] = None
-    ###### DO NOT EDIT ANYTHING IN THIS CLASS ######
-
 
 class subBoard(Board):
     def __init__(self, rsize: int, csize: int, db):
