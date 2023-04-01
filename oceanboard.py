617a33e227fd909d14637d3eb57ed87cbbea7c2d
RyanChew1 <113740030+RyanChew1@users.noreply.github.com>
2023-03-31 23:07:37 -0700

    Update oceanboard.py

--- oceanboard.py
+++ oceanboard.py
@@ -1,11 +1,7 @@
 from board import Board
 from ship import Ship
 
-'''
-The Ocean Board is the portion of the Battleship Unit that holds
-ships that the player had placed and keeps track of hits upon each
-ship that the opponent has guessed correctly.
-'''
+
 class OceanBoard(Board):
 
     def __init__(self, rsize: int, csize: int):
@@ -13,12 +9,7 @@ class OceanBoard(Board):
         super().__init__(rsize, csize)
         self.ships = []
 
-    # return False if ship could not be placed at r, c
-    #    either r, c are illegal
-    #    or ship is too big to be placed there
-    #    or another ship is already there
-    # Modify 'ship' with the given (r, c) and orientation
-    # Add 'ship' to the the OceanBoard's list of 'ships' if placed successfully
+
     def placeShip(self, ship: Ship, r: int, c: int, orientation: str) -> bool:
         if r < 0 or r >= self.rowSize() or c < 0 or c >= self.colSize():
             return False
@@ -52,7 +43,6 @@ class OceanBoard(Board):
         self.ships.append(ship)
         return True
 
-    # are all 'ships' in the OceanBoard sunk?
     def allShipsSunk(self):
         for ship in self.ships:
             if not ship.isSunk():
